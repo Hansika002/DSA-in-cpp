7173f3c6d90e8c97460c6495392c2bb8dbbd2f2b
hansika <hansika.amrutkar@gmail.com>
2024-02-16 23:40:08 +0530

    change in file

--- Bubble Sort/bubblesort.cpp
+++ Bubble Sort/bubblesort.cpp
@@ -3,6 +3,7 @@ using namespace std;
 void bubbleSort(vector<int>& arr, int n)
 {   
     for(int i = 1; i<n; i++) {
+        //for round 1 to n-1
         bool swapped = false;
         
         for(int j =0; j<n-i; j++) {
