6f32aad940be7a8f371fd95c6be64067349b5546
hansika <hansika.amrutkar@gmail.com>
2024-02-28 23:49:01 +0530

    updated code

--- Strings/reverseString.cpp
+++ Strings/reverseString.cpp
@@ -7,10 +7,10 @@ class Solution {
 public:
 
     void reverseString(vector<char>& s) {
-        int st=0;
+        int st = 0;
         int e = s.size()-1;
 
-        while(st<e) {
+        while(st < e) {
             swap(s[st++], s[e--]);
         }
     }
